dbf885218c909336d5d47c118add29a3d3b6c31c
EmmanuelER <emmanuel@neuronmexico.com>
2021-05-09 03:29:11 -0500

    added firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+import { environment } from '../environments/environment';
+
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+    AngularFirestoreModule, // firestore
+    AngularFireAuthModule, // auth
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: 'AIzaSyBxkHZesuN9fvN1CEsu6ISNc4P6sugspHk',
+    authDomain: 'task-57a34.firebaseapp.com',
+    projectId: 'task-57a34',
+    storageBucket: 'task-57a34.appspot.com',
+    messagingSenderId: '510492498362',
+    appId: '1:510492498362:web:587696779a7ffb78102583',
+    measurementId: 'G-HJ08C9QVS3',
+  },
 };
