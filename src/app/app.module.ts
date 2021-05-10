81d438a59ec9cd1983d87a99de731cd907ed090b
EmmanuelER <emmanuel@neuronmexico.com>
2021-05-10 10:53:31 -0500

    added new component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,9 +8,10 @@ import { AngularFireModule } from '@angular/fire';
 import { AngularFirestoreModule } from '@angular/fire/firestore';
 import { AngularFireAuthModule } from '@angular/fire/auth';
 import { environment } from '../environments/environment';
+import { LoginComponent } from './pages/login/login.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, LoginComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
